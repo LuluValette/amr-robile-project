4e45b8f5d7ac2f160d3e8331c42c9a411338617a
LuluValette <valetteluc14@gmail.com>
2026-03-12 15:49:07 +0100

    Modify amr_project.launch.py

--- src/amr_project_lvalet2s_yakter2s/launch/amr_project.launch.py
+++ src/amr_project_lvalet2s_yakter2s/launch/amr_project.launch.py
@@ -1,5 +1,6 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.actions import TimerAction
 from ament_index_python.packages import get_package_share_directory
 import os
 
@@ -16,40 +17,60 @@ def generate_launch_description():
         'maps', 'closed_walls_map.yaml'
     )
 
-    return LaunchDescription([
-        Node(
-            package='nav2_map_server',
-            executable='map_server',
-            name='map_server',
-            output='screen',
-            parameters=[{'yaml_filename': map_file, 'use_sim_time': False}]
-        ),
-        Node(
-            package='nav2_lifecycle_manager',
-            executable='lifecycle_manager',
-            name='lifecycle_manager_map',
-            output='screen',
-            parameters=[{'autostart': True, 'node_names': ['map_server']}]
-        ),
-        Node(
+    map_server = Node(
+        package='nav2_map_server',
+        executable='map_server',
+        name='map_server',
+        output='screen',
+        parameters=[{'yaml_filename': map_file, 'use_sim_time': False}]
+    )
+
+    lifecycle_manager = Node(
+        package='nav2_lifecycle_manager',
+        executable='lifecycle_manager',
+        name='lifecycle_manager_map',
+        output='screen',
+        parameters=[{'autostart': True, 'node_names': ['map_server']}]
+    )
+
+    particle_filter = TimerAction(
+        period=3.0,
+        actions=[Node(
             package=package_name,
             executable='particle_filter_localization',
             name='particle_filter_localization',
             parameters=[part2_config],
             output='screen',
-        ),
-        Node(
+        )]
+    )
+
+    path_planner = TimerAction(
+        period=4.0,
+        actions=[Node(
             package=package_name,
             executable='path_motion_planner',
             name='path_motion_planner',
             parameters=[part1_config],
             output='screen',
-        ),
-        Node(
+        )]
+    )
+
+    frontier_explorer = TimerAction(
+        period=5.0,
+        actions=[Node(
             package=package_name,
             executable='frontier_explorer',
             name='frontier_explorer',
             parameters=[part3_config],
             output='screen',
-        ),
+        )]
+    )
+
+    return LaunchDescription([
+        map_server,
+        lifecycle_manager,
+        particle_filter,
+        path_planner,
+        frontier_explorer,
     ])
+
